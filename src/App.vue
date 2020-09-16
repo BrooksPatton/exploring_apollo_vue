fdde5b16a67ba84f31400b92625ef1d3f4a9a8fa
Brooks Patton <brooks.patton@healthscholars.com>
2020-09-16 11:21:41 -0600

    got basic apollo graphql query working

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,36 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{listFacilitys}}
+
+    {{listCourse_packages}}
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+// brought in gql for apollo stuff
+import gql from 'graphql-tag'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  apollo: {
+    // Simple query that will update the 'hello' vue property
+    listFacilitys: gql`query {
+      listFacilitys {
+        id
+        name
+      }
+    }`,
+    listCourse_packages: gql`query {
+      listCourse_packages {
+        name
+      }
+    }`,
+  },
 }
 </script>
 
